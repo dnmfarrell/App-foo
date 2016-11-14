f5318247341adbe7b3ff915c040113d74357e817
David Farrell <davidnmfarrell@gmail.com>
2016-11-14 09:26:35 -0500

    initial commit of base files

---
+++ lib/App/foo.pm
@@ -0,0 +1,23 @@
+package App::foo;
+
+our $VERSION = 0.01;
+
+=head1 NAME
+
+App::foo - an app that prints "foo!"
+
+=head1 DESCRIPTION
+
+This is a stub module, see F<script/foo> for details of the app.
+
+=head1 AUTHOR
+
+David Farrell
+
+=head1 LICENSE
+
+FreeBSD
+
+=cut
+
+1;
